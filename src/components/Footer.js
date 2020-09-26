414cfa6fff55b93ccc23b8b856e869f8fd6e14ae
Jason A. Wise <jasonandrewwise@gmail.com>
2020-09-26 13:18:59 -0400

    adding font awesome and social links to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,15 @@
 /* eslint-disable react/jsx-no-literals */
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faTwitter, faFacebookF } from '@fortawesome/free-brands-svg-icons'
+
 
 const Footer = props => {
+   const [ isModalOpen, setIsModalOpen ] = useState( false )
    const handleClick = () => {
       //  @TODO show model with content
-      alert( `clicked` )
+      setIsModalOpen( true )
    }
 
    return (
@@ -16,6 +20,11 @@ const Footer = props => {
          >
             {`About`}
          </a>
+         <ul className="flex justify-center mt-6">
+            <li><a href="https://github.com/jasonawise" target="_blank"><FontAwesomeIcon icon={faGithub} size="lg" /></a></li>
+            <li className="mx-5"><a href="https://twitter.com/wiseprogrammer" target="_blank"><FontAwesomeIcon icon={faTwitter} size="lg" /></a></li>
+            <li><a href="https://www.facebook.com/wisedeveloper" target="_blank"><FontAwesomeIcon icon={faFacebookF} size="lg" /></a></li>
+         </ul>
          <p
             className="mt-6"
          >
