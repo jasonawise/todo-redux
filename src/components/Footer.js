bb795c5580bb45b663a211ff5d0a722a0f909b68
Jason A. Wise <jasonandrewwise@gmail.com>
2020-09-26 12:34:35 -0400

    adding border to link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,6 +11,7 @@ const Footer = props => {
    return (
       <footer className="text-center p-6 mt-8 bg-orange-800 text-gray-100 absolute inset-x-0 bottom-0">
          <a
+            className="border-b"
             href=""
             onClick={handleClick}
          >
